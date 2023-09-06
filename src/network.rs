7cf18163ae4f77c888d7de0f8a62d0389d290d16
Oleg Keri <ezhi99@gmail.com>
2023-09-06 17:15:33 +0200

    replace raw parsing of /proc/net/wireless with iwlib

--- src/network.rs
+++ src/network.rs
@@ -1,5 +1,6 @@
 use super::base::{Base, Value};
 use super::block;
+use iwlib;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -20,23 +21,10 @@ fn default_wifi() -> String {
     "wifi".to_string()
 }
 
-fn parse_wifi_str(interface: &str, text: String) -> Result<u32, ()> {
-    for line in text.split('\n') {
-        if line.contains(interface) {
-            let linedata: Vec<&str> = line.split_whitespace().collect();
-            return linedata[2][0..linedata[2].len() - 1]
-                .parse::<u32>()
-                .map(|value| value * 100 / 70)
-                .map_err(|_| ());
-        }
-    }
-    Err(())
-}
-
-fn get_wifi_str(interface: &str) -> Result<u32, ()> {
-    std::fs::read_to_string("/proc/net/wireless")
-        .map_err(|_| ())
-        .and_then(|text| parse_wifi_str(interface, text))
+fn get_wifi_strength(interface: &str) -> Result<u32, ()> {
+    iwlib::get_wireless_info(interface)
+        .ok_or(())
+        .map(|info| info.wi_quality.into())
 }
 
 fn get_active_interface() -> Result<String, ()> {
@@ -66,7 +54,7 @@ impl block::Block for Block {
     fn update(&mut self) {
         self.base.value = Value::Invalid;
         if let Ok(iface) = get_active_interface() {
-            if let Ok(strength) = get_wifi_str(&iface) {
+            if let Ok(strength) = get_wifi_strength(&iface) {
                 self.base.set_prefix(&self.wifi);
                 self.base.set_suffix("%");
                 self.base.value = Value::new(strength);
